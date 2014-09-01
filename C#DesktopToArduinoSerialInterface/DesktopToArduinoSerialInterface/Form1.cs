6b5537bae6e0702c270422eae35f401249b5b5df
Jonathan75 <jonathanstevens75@gmail.com>
2014-09-01 08:52:29 -0400

    C# desktop software to send messages to Arduino

---
+++ C#DesktopToArduinoSerialInterface/DesktopToArduinoSerialInterface/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DesktopToArduinoSerialInterface
+{
+	public partial class Form1 : Form
+	{
+		SerialPortWrapper serialPortWrapper;
+
+		public Form1()
+		{
+			InitializeComponent();
+			serialPortWrapper = new SerialPortWrapper();
+		}
+
+		
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+
+			serialPortWrapper.Send(textBox1.Text);
+		}
+
+	
+	}
+
+	public class SerialPortWrapper
+	{
+		public SerialPort SerialPort;
+		public SerialPortWrapper()
+		{
+			SerialPort = new SerialPort() { PortName = "COM4", BaudRate = 9600 };
+			
+		}
+
+		public void Send(string message)
+		{
+			if (SerialPort.IsOpen) return;
+
+			try
+			{
+				SerialPort.Open();
+				SerialPort.Write(message);
+
+			}
+			catch (Exception ex)
+			{
+				throw;
+			}
+			finally
+			{
+				SerialPort.Close();
+			}
+		}
+	}
+}
