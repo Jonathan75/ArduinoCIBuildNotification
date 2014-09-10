9671381197eb3738dbacc495a1d7bde819d6b511
Jonathan75 <jonathanstevens75@gmail.com>
2014-09-10 16:36:49 -0400

    successfully connected to TFS 2010

--- C#DesktopToArduinoSerialInterface/DesktopToArduinoSerialInterface/Form1.Designer.cs
+++ C#DesktopToArduinoSerialInterface/DesktopToArduinoSerialInterface/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace DesktopToArduinoSerialInterface
+﻿using System.Windows.Forms;
+
+namespace DesktopToArduinoSerialInterface
 {
 	partial class Form1
 	{
@@ -29,13 +31,16 @@
 		private void InitializeComponent()
 		{
 			this.button1 = new System.Windows.Forms.Button();
-			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.txtLightDelay = new System.Windows.Forms.TextBox();
 			this.lstPorts = new System.Windows.Forms.ComboBox();
+			this.lstProject = new System.Windows.Forms.ComboBox();
+			this.lstBuildDefinitions = new System.Windows.Forms.ComboBox();
+			this.lstBuilds = new System.Windows.Forms.ListBox();
 			this.SuspendLayout();
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(86, 150);
+			this.button1.Location = new System.Drawing.Point(86, 227);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(75, 23);
 			this.button1.TabIndex = 0;
@@ -43,31 +48,61 @@
 			this.button1.UseVisualStyleBackColor = true;
 			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
-			// textBox1
+			// txtLightDelay
 			// 
-			this.textBox1.Location = new System.Drawing.Point(86, 107);
-			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(100, 20);
-			this.textBox1.TabIndex = 1;
-			this.textBox1.Text = "50";
+			this.txtLightDelay.Location = new System.Drawing.Point(86, 201);
+			this.txtLightDelay.Name = "txtLightDelay";
+			this.txtLightDelay.Size = new System.Drawing.Size(100, 20);
+			this.txtLightDelay.TabIndex = 1;
+			this.txtLightDelay.Text = "50";
+			this.txtLightDelay.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtLightDelay_KeyDown);
 			// 
 			// lstPorts
 			// 
 			this.lstPorts.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
 			this.lstPorts.FormattingEnabled = true;
-			this.lstPorts.Location = new System.Drawing.Point(86, 69);
+			this.lstPorts.Location = new System.Drawing.Point(86, 174);
 			this.lstPorts.Name = "lstPorts";
 			this.lstPorts.Size = new System.Drawing.Size(121, 21);
 			this.lstPorts.TabIndex = 2;
 			this.lstPorts.SelectedIndexChanged += new System.EventHandler(this.lstPorts_SelectedIndexChanged);
+			// 
+			// lstProject
+			// 
+			this.lstProject.FormattingEnabled = true;
+			this.lstProject.Location = new System.Drawing.Point(86, 147);
+			this.lstProject.Name = "lstProject";
+			this.lstProject.Size = new System.Drawing.Size(121, 21);
+			this.lstProject.TabIndex = 3;
+			this.lstProject.SelectedIndexChanged += new System.EventHandler(this.lstProject_SelectedIndexChanged);
+			// 
+			// lstBuildDefinitions
+			// 
+			this.lstBuildDefinitions.FormattingEnabled = true;
+			this.lstBuildDefinitions.Location = new System.Drawing.Point(86, 12);
+			this.lstBuildDefinitions.Name = "lstBuildDefinitions";
+			this.lstBuildDefinitions.Size = new System.Drawing.Size(121, 21);
+			this.lstBuildDefinitions.TabIndex = 4;
+			this.lstBuildDefinitions.SelectedIndexChanged += new System.EventHandler(this.lstBuildDefinitions_SelectedIndexChanged);
+			// 
+			// lstBuilds
+			// 
+			this.lstBuilds.FormattingEnabled = true;
+			this.lstBuilds.Location = new System.Drawing.Point(86, 39);
+			this.lstBuilds.Name = "lstBuilds";
+			this.lstBuilds.Size = new System.Drawing.Size(120, 95);
+			this.lstBuilds.TabIndex = 5;
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.Controls.Add(this.lstBuilds);
+			this.Controls.Add(this.lstBuildDefinitions);
+			this.Controls.Add(this.lstProject);
 			this.Controls.Add(this.lstPorts);
-			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.txtLightDelay);
 			this.Controls.Add(this.button1);
 			this.Name = "Form1";
 			this.Text = "Form1";
@@ -79,8 +114,11 @@
 		#endregion
 
 		private System.Windows.Forms.Button button1;
-		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox txtLightDelay;
 		private System.Windows.Forms.ComboBox lstPorts;
+		private ComboBox lstProject;
+		private ComboBox lstBuildDefinitions;
+		private ListBox lstBuilds;
 	}
 }
 
--- C#DesktopToArduinoSerialInterface/DesktopToArduinoSerialInterface/Form1.cs
+++ C#DesktopToArduinoSerialInterface/DesktopToArduinoSerialInterface/Form1.cs
@@ -7,22 +7,32 @@ using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+//using DesktopToArduinoSerialInterface.TfsBuildWebService;
+using Microsoft.TeamFoundation.Build.Client;
+using Microsoft.TeamFoundation.Client;
+//using Microsoft.TeamFoundation.Framework.Common;
+//using Microsoft.TeamFoundation.VersionControl.Client;
 
 namespace DesktopToArduinoSerialInterface
 {
 	public partial class Form1 : Form
 	{
 		SerialPortWrapper serialPortWrapper;
-
+		TfsWrapper tfsWrapper;
 		public Form1()
 		{
 			InitializeComponent();
 			serialPortWrapper = new SerialPortWrapper();
 
 			lstPorts.DataSource = serialPortWrapper.GetPorts();
-			//lstPorts.DataSource;
 
-			//this.Text = serialPortWrapper.GetPorts();
+			tfsWrapper = new TfsWrapper();
+
+
+			lstBuildDefinitions.DataSource = tfsWrapper.GetAllBuildDefinitionsFromTheTeamProject();
+			lstBuildDefinitions.DisplayMember = "Name";
+			lstBuildDefinitions.ValueMember  = "Uri";
+			//lstBuildDefinitions.SelectedIndex = 0;
 		}
 
 		private void lstPorts_SelectedIndexChanged(object sender, EventArgs e)
@@ -33,10 +43,74 @@ namespace DesktopToArduinoSerialInterface
 		private void button1_Click(object sender, EventArgs e)
 		{
 
-			serialPortWrapper.Send(textBox1.Text);
+			updateLightDelay();
 		}
 
+		private void txtLightDelay_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Enter)
+			{
+				updateLightDelay();
+			}
+		}
+
+
+		void updateLightDelay()
+		{
+			serialPortWrapper.Send(txtLightDelay.Text);
+		}
+
+		private void lstProject_SelectedIndexChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void lstBuildDefinitions_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			var buildDefinition = (((ComboBox)sender).SelectedItem) as IBuildDefinition;
+
+			if (buildDefinition == null) 
+				return;
+			
+			lstBuilds.DataSource= tfsWrapper.Builds(buildDefinition.Name);
+			lstBuilds.DisplayMember = "BuildNumber";
+			lstBuilds.ValueMember = "Uri";
+			//lstBuilds.SelectedIndex = 0;
+		}
+
+	}
 	
+	public class TfsWrapper
+	{
+		TfsTeamProjectCollection _tfs;
+		string _selectedTeamProject;
+		IBuildServer _bs;
+		
+		public TfsWrapper()
+		{
+
+			TeamProjectPicker tfsPP = new TeamProjectPicker(TeamProjectPickerMode.SingleProject, false);
+			tfsPP.ShowDialog();
+			_tfs = tfsPP.SelectedTeamProjectCollection;
+			_selectedTeamProject = tfsPP.SelectedProjects[0].Name;
+			_bs = _tfs.GetService<IBuildServer>();
+		}
+
+		public IBuildDefinition[] GetAllBuildDefinitionsFromTheTeamProject()
+		{
+			return _bs.QueryBuildDefinitions(_selectedTeamProject);
+		}
+
+		public IBuildDetail[] Builds(string buildName)
+		{
+			var def = _bs.CreateBuildDetailSpec(_selectedTeamProject);
+			def.MaxBuildsPerDefinition = 10;
+			def.QueryOrder = BuildQueryOrder.FinishTimeDescending;
+			def.DefinitionSpec.Name = buildName;
+			def.Status = BuildStatus.InProgress;
+			return _bs.QueryBuilds(def).Builds;
+		}
+		
 	}
 
 	public class SerialPortWrapper
